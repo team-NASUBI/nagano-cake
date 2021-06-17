399fc21a974a0b495f23cce04db4fbcdd1cc5b96
SHINYATANAKA123 <123asde.med@gmail.com>
2021-06-17 09:58:32 +0000

    [Add] order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,15 @@
 class Admin::OrdersController < ApplicationController
   def index
+    @orders = Order.page(params[:page]).per(10)
   end
 
   def show
+    @shipping = 800
+    @order = Order.find_by(id: params[:id])
+    if @order
+      @order_products = OrderProduct.where(order_id: @order.id)
+    else
+      redirect_to admin_customer_path(params[:id])
+    end
   end
 end
