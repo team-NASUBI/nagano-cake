197ca64010474a1d6dcaccf3abf257149abe6ce0
AyamiYokoi <didufd63hff@gmail.com>
2021-06-17 06:48:00 +0000

    [Add]商品新規登録ページ

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,11 +3,31 @@ class Admin::ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
-
+  
+  def create
+    product = Product.new(product_params)
+    product.save
+    redirect_to admin_product_path(product.id)
+  end
+  
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+ 
+  def update
+    product = Product.find(params[:id])
+    product.update(product_params)
+    redirect_to admin_product_path(product.id)
+  end
+  
+  private
+  def product_params
+    params.require(:product).permit(:name, :introduction, :product_image, :genre, :price, :now_on_sale) 
   end
 end
