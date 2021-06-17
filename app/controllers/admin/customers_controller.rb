80a1eaa7279eed5b3514daad13299d2c0e485d4b
SHINYATANAKA123 <123asde.med@gmail.com>
2021-06-17 12:36:29 +0000

    [Add] 管理側の顧客機能

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,29 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "会員情報を変更しました。"
+      redirect_to admin_customers_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def customer_params
+     params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name,
+                                      :email, :withdrawal_status, :postal_code, :address, :telephone_number)
   end
 end
