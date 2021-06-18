f87281a2b9b64e1647c8c17ca48e82bf4fe81a26
shin-ito0306 <shin.20000306@icloud.com>
2021-06-18 04:31:55 +0000

    [update]controller create

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -3,7 +3,23 @@ class Public::ShippingAddressesController < ApplicationController
     @shipping_address = ShippingAddress.new
     @shipping_addresses = ShippingAddress.where(customer_id: current_customer.id)
   end
+  
+  def create
+    @shipping_address = ShippingAddress.new(shipping_address_params)
+    @shipping_address.customer_id = current_customer.id
+    if @shipping_address.save
+      redirect_to shipping_addresses_path
+    else
+      render :index
+    end
+  end
+  
 
   def edit
+  end
+  
+  private
+  def shipping_address_params
+    params.require(:shipping_address).permit(:shipping_name, :postal_code, :address)
   end
 end
