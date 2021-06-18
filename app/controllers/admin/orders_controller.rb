8a27a6e2420e4d9b9e869e5edc581a30c633093e
YutakaAihara <youguisufanyuan@gmail.com>
2021-06-18 14:15:56 +0000

    休憩

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -12,4 +12,34 @@ class Admin::OrdersController < ApplicationController
       redirect_to admin_customer_path(params[:id])
     end
   end
+  
+  def new
+    @order = Order.new
+  end
+  
+  def confirm
+    @order_products = session[:cart_id].map{ |cart_id| Cart.find(cart_id) }
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.shipping = 800
+    @order.payment_method = params[:order][:payment_method]
+    @order.total_price = @order_products.total
+    
+    if params[:order][:address_option] == 0
+      
+    
+  
+  end
+  
+  def create
+
+  end
+  
+    
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:name, :address, :postal_code, :total_price, :payment_method)
+  end
 end
