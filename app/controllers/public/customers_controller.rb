0ead733d55a3966bf338f182016a6ff6deb5a0ce
shin-ito0306 <shin.20000306@icloud.com>
2021-06-18 08:40:14 +0000

    [finish]edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,8 +4,23 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path(current_customer.id)
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :telephone_number)
   end
 end
