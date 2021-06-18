5b71453ebeab6c5c2b995ad73ac265ff002224a6
shin-ito0306 <shin.20000306@icloud.com>
2021-06-18 03:54:45 +0000

    [finish]index

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,7 +1,7 @@
 class Public::ShippingAddressesController < ApplicationController
   def index
     @shipping_address = ShippingAddress.new
-    @shipping_addresses = ShippingAddress.all
+    @shipping_addresses = ShippingAddress.where(customer_id: current_customer.id)
   end
 
   def edit
