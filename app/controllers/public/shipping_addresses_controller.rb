eda10528aedd65dc35bec58be8b850c76289a9b0
shin-ito0306 <shin.20000306@icloud.com>
2021-06-18 04:07:38 +0000

    [add]edit_controller

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -5,5 +5,16 @@ class Public::ShippingAddressesController < ApplicationController
   end
 
   def edit
+    @shipping_address = ShippingAddress.find(params[:id])
   end
+  
+  def update
+    @shipping_address = ShippingAddress.find(params[:id])
+    if @shipping_address.update(shipping_address_params)
+      redirect_to shipping_addresses_path
+    else
+      render :edit
+    end
+  end
+  
 end
