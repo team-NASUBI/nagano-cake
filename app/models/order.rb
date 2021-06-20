d4947dd14d3ae8062a9c76d0f4e08cfaa35cad81
YutakaAihara <youguisufanyuan@gmail.com>
2021-06-20 06:31:07 +0000

    会員側注文履歴・注文履歴詳細完成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  has_many :order_products, dependent: :destroy
 
-
+  enum payment_method: {credit:0, bank:1}
+  enum status: {nyukinmati:0, nyukinkakunin:1,  seisakutyu:2, hassoujunbityu:3, hassouzumi:4}
+  
   def self.total(cart_products)
     a = []
 
@@ -11,14 +14,4 @@ class Order < ApplicationRecord
 
     (a.sum * 1.1).floor
   end
-  
-  
-
-  has_many :order_products, dependent: :destroy
-
- 
-
-  
-  enum payment_method: {クレジットカード:0, 銀行振込:1}
-  enum order_status: {入金待ち:0, 入金確認:1,  製作中:2, 発送準備中:3, 発送済み:4}
 end
