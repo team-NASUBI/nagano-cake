abb9685b86937fe0c92b753fe0f27388c1e31cdc
YutakaAihara <youguisufanyuan@gmail.com>
2021-06-21 01:21:59 +0000

    [Add]バリデーション等追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,18 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_products, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :address, presence: true
+  VALID_POSTAL_CODE_REGEX = /\A\d{7}\z/
+  validates :postal_code, presence: true, length: { is: 7 }, format: { with: VALID_POSTAL_CODE_REGEX }
+  validates :shipping, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :total_price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :payment_method, presence: true
+  validates :status, presence: true
 
   enum payment_method: {credit:0, bank:1}
-  enum status: {nyukinmati:0, nyukinkakunin:1,  seisakutyu:2, hassoujunbityu:3, hassouzumi:4}
+  enum status: {waiting_for_payment:0, payment_confirmation:1, in_production:2, preparing_to_ship:3, shipped:4}
   
   def self.total(cart_products)
     a = []
@@ -14,4 +23,6 @@ class Order < ApplicationRecord
 
     (a.sum * 1.1).floor
   end
+  
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,13 @@ class Product < ApplicationRecord
   belongs_to :genre
   has_many :carts, dependent: :destroy
   attachment :product_image
+  
+  validates :name, presence: true
+  validates :product_image_id, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :now_on_sale, presence: true
+  
 
   def include_tax
     (self.price * 1.1).floor
