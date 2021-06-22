c830185eaafeb1ca4ba010b1fa3a35b40fe1250a
SHINYATANAKA123 <123asde.med@gmail.com>
2021-06-22 10:01:59 +0000

    [Update] 複数のカラムに対しても検索を対応

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,7 +8,7 @@ class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :shipping_addresses, dependent: :destroy
 
-  
+
   validates :postal_code, presence: true, length: { is: 7 }
   validates :address, presence: true
   validates :first_name, presence: true
@@ -16,14 +16,22 @@ class Customer < ApplicationRecord
   validates :kana_first_name, presence: true
   validates :kana_last_name, presence: true
   validates :telephone_number, presence: true
-  
+
   def full_name(customer)
     customer.last_name + customer.first_name
   end
 
-  def self.search(search)
-      Customer.where(['id LIKE(?) OR first_name LIKE(?) OR withdrawal_status LIKE(?) OR last_name LIKE(?) OR kana_first_name LIKE(?) OR kana_last_name LIKE(?)',
-                       "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
-  end
+   def self.search(word)
+    if word =~  /^[0-9]+$/
+      @search_customers = Customer.where("id LIKE?","%#{word}%")
+    elsif word == "false" || word == "有効"
+      @search_customers = Customer.where(withdrawal_status: false)
+    elsif word == "true" || word == "退会"
+      @search_customers = Customer.where(withdrawal_status: true)
+    else
+      @search_customers = Customer.where("first_name LIKE? OR last_name LIKE? OR kana_first_name LIKE? OR kana_last_name LIKE? OR email LIKE?",
+                                        "%#{word}%","%#{word}%","%#{word}%","%#{word}%","%#{word}%")
+    end
+   end
 
 end
